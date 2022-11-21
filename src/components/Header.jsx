7d9da99514d455c5cb232120494d019c8b4cb863
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2022-11-21 13:19:29 +0100

    Header done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,75 @@
+import "../styles/Header.scss";
+
 function Header() {
-  return <header>Header</header>;
+  return (
+    <header>
+      <nav className="navbar">
+        <svg height="50" width="50">
+          <circle
+            cx="25"
+            cy="25"
+            r="20"
+            stroke="none"
+            stroke-width="4"
+            fill="#226F63"
+          />
+          <text
+            fill="#ffffff"
+            font-size="12"
+            font-family="Verdana"
+            x="12"
+            y="30"
+          >
+            CO2
+          </text>
+          Sorry, your browser does not support inline SVG.
+        </svg>
+        <ul>
+          <li>
+            <a href="">Første</a>
+          </li>
+          <li>
+            <a href="">Anden</a>
+          </li>
+          <li>
+            <a href="">Trejde</a>
+          </li>
+        </ul>
+      </nav>
+      <section className="max-width" id="headerSection">
+        <div className="textContent">
+          <h1>CO2 UDREGNER</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nihil,
+            officiis et. Placeat, tempore. Odio ea et ipsa deserunt nesciunt
+            minima sint deleniti iure unde aliquam.
+          </p>
+          <button>CTA</button>
+        </div>
+        <div className="circler">
+        <svg height="320" width="320" className="greenCircle">
+          <circle
+            cx="160"
+            cy="160"
+            r="160"
+            stroke="none"
+            stroke-width="4"
+            fill="#226F63"
+          />
+        </svg>
+        <svg height="320" width="320" className="orangeCircle">
+          <circle
+            cx="160"
+            cy="160"
+            r="160"
+            stroke="none"
+            stroke-width="4"
+            fill="#e46f21"
+          />
+        </svg></div>
+      </section>
+    </header>
+  );
 }
 
 export default Header;
