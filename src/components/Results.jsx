f27a9a14cbfe8be8fb23a5834c15b7714f0f75e2
rebe7917 <rebe7917@stud.kea.dk>
2022-11-21 14:40:03 +0100

    a little styling on result

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,23 +1,36 @@
+import "../styles/results.scss"
+
 function Results() {
   return (
     <section id="results" className="max-width">
-      <h2>Dette er en overskrift</h2>
-      <select name="visualisering" id="visualisering">
-        <option value="power"></option>
-        <option value="cigarettes"></option>
-        <option value="coffeecups"></option>
-      </select>
-      <div>
-        <h4>Daily</h4>
-        <div className="daily"></div>
-        <h4>Monthly</h4>
-        <div className="monthly"></div>
-        <h4>Yearly</h4>
-        <div className="yearly"></div>
-        <div className="trees"></div>
+      <div className="visuel-results">
+        <div className="result-text">
+          <h2>Dette er en overskrift</h2>
+          <select name="visualisering" id="visualisering">
+            <option value="power" selected>
+              Power
+            </option>
+            <option value="cigarettes">Cigarettes</option>
+            <option value="coffeecups">Cups of coffee</option>
+          </select>
+        </div>
+        <div className="visuel">
+          <div>
+            <h4>Daily</h4>
+            <div className="daily"></div>
+          </div>
+          <div>
+            <h4>Monthly</h4>
+            <div className="monthly"></div>
+          </div>
+          <div>
+            <h4>Yearly</h4>
+            <div className="yearly"></div>
+          </div>
+        </div>
       </div>
       <div>
-        <p>For at udligne dit forburg af co2 skal du plante følgende antal træer: </p>
+        <p>For at udligne dit co2 udledning skal du plante følgende antal træer: </p>
         <p>Dagligt:</p>
         <p>Månedligt:</p>
         <p>Årligt:</p>
