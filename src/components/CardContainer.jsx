9a8f6f3c5adf27130de24b14bef1f7cfc3656f30
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2022-11-23 09:52:09 +0100

    add svg src paths

--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -2,17 +2,77 @@ import Card from "./Card";
 
 function CardContainer({ onChange }) {
   const cards = [
-    { index: 0, title: "Instagram", icon: "", onChange: onChange, ratio: 1.05 },
-    { index: 1, title: "Twitter", icon: "", onChange: onChange, ratio: 0.6 },
-    { index: 2, title: "Reddit", icon: "", onChange: onChange, ratio: 2.48 },
-    { index: 3, title: "Twitch", icon: "", onChange: onChange, ratio: 0.55 },
-    { index: 4, title: "Snapchat", icon: "", onChange: onChange, ratio: 0.87 },
-    { index: 5, title: "Pinterest", icon: "", onChange: onChange, ratio: 1.3 },
-    { index: 6, title: "LinkedIn", icon: "", onChange: onChange, ratio: 0.71 },
-    { index: 7, title: "YouTube", icon: "", onChange: onChange, ratio: 0.46 },
+    {
+      index: 0,
+      title: "Instagram",
+      icon: "src/assets/icons/icons8-instagram.svg",
+      onChange: onChange,
+      ratio: 1.05,
+    },
+    {
+      index: 1,
+      title: "Twitter",
+      icon: "src/assets/icons/icons8-twitter.svg",
+      onChange: onChange,
+      ratio: 0.6,
+    },
+    {
+      index: 2,
+      title: "Reddit",
+      icon: "src/assets/icons/reddit-seeklogo.com.svg",
+      onChange: onChange,
+      ratio: 2.48,
+    },
+    {
+      index: 3,
+      title: "Twitch",
+      icon: "src/assets/icons/icons8-twitch.svg",
+      onChange: onChange,
+      ratio: 0.55,
+    },
+    {
+      index: 4,
+      title: "Snapchat",
+      icon: "src/assets/icons/icons8-snapchat.svg",
+      onChange: onChange,
+      ratio: 0.87,
+    },
+    {
+      index: 5,
+      title: "Pinterest",
+      icon: "src/assets/icons/icons8-pinterest.svg",
+      onChange: onChange,
+      ratio: 1.3,
+    },
+    {
+      index: 6,
+      title: "LinkedIn",
+      icon: "src/assets/icons/icons8-linkedin.svg",
+      onChange: onChange,
+      ratio: 0.71,
+    },
+    {
+      index: 7,
+      title: "YouTube",
+      icon: "src/assets/icons/icons8-youtube.svg",
+      onChange: onChange,
+      ratio: 0.46,
+    },
     { index: 8, title: "Streaming", icon: "", onChange: onChange, ratio: 0.92 },
-    { index: 9, title: "TikTok", icon: "", onChange: onChange, ratio: 0.63 },
-    { index: 10, title: "Facebook", icon: "", onChange: onChange, ratio: 0.79 },
+    {
+      index: 9,
+      title: "TikTok",
+      icon: "src/assets/icons/icons8-tiktok.svg",
+      onChange: onChange,
+      ratio: 0.63,
+    },
+    {
+      index: 10,
+      title: "Facebook",
+      icon: "src/assets/icons/icons8-facebook-240.svg",
+      onChange: onChange,
+      ratio: 0.79,
+    },
   ];
 
   const listItems = cards.map((card) => (
