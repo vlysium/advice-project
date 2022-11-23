d28f2e4802053ab85803f5dbd516ecfc7a04cdaf
rebe7917 <rebe7917@stud.kea.dk>
2022-11-23 15:26:12 +0100

    alot of stuff

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -11,6 +11,18 @@ function Results({ CO2Total, dynamicValue, onUpdate, setVisualization, unit }) {
     setVisualization(event.target.value);
     onUpdate(event.target.value);
   }
+  function image() {
+    switch (unit) {
+      case "power":
+        return "light-bulb.svg";
+
+      case "cigarettes":
+        return "cig.svg";
+
+      case "coffeecups":
+        return "coffee.svg";
+    }
+  }
 
   return (
     <section id="results">
@@ -20,23 +32,10 @@ function Results({ CO2Total, dynamicValue, onUpdate, setVisualization, unit }) {
           <DropDown onChange={onChange} />
         </div>
         <div className="visuel max-width">
+          <img src={"./assets/icons/" + image()} /* className={myClassName} */ alt={unit}></img>
           <Usage title="Daily" myClassName="daily" time="day" dynamicValue={dynamicValue} ratio="1" unit={unit} />
-          <Usage
-            title="Monthly"
-            myClassName="monthly"
-            time="month"
-            dynamicValue={dynamicValue}
-            ratio="30.42"
-            unit={unit}
-          />
-          <Usage
-            title="Yearly"
-            myClassName="yearly"
-            time="year"
-            dynamicValue={dynamicValue}
-            ratio="365.25"
-            unit={unit}
-          />
+          <Usage title="Monthly" myClassName="monthly" time="month" dynamicValue={dynamicValue} ratio="30.42" unit={unit} />
+          <Usage title="Yearly" myClassName="yearly" time="year" dynamicValue={dynamicValue} ratio="365.25" unit={unit} />
         </div>
       </div>
       <div className="tree-grid max-width">
@@ -47,14 +46,7 @@ function Results({ CO2Total, dynamicValue, onUpdate, setVisualization, unit }) {
           <TreesPlanted title="Yearly" ratio="365.25" CO2Total={CO2Total} />
         </article>
         <div className="tree-svg">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            maxwidth="500"
-            height="300"
-            fill="green"
-            className="bi bi-tree"
-            viewBox="0 0 16 16"
-          >
+          <svg xmlns="http://www.w3.org/2000/svg" maxwidth="500" height="300" fill="green" className="bi bi-tree" viewBox="0 0 16 16">
             <path d="M8.416.223a.5.5 0 0 0-.832 0l-3 4.5A.5.5 0 0 0 5 5.5h.098L3.076 8.735A.5.5 0 0 0 3.5 9.5h.191l-1.638 3.276a.5.5 0 0 0 .447.724H7V16h2v-2.5h4.5a.5.5 0 0 0 .447-.724L12.31 9.5h.191a.5.5 0 0 0 .424-.765L10.902 5.5H11a.5.5 0 0 0 .416-.777l-3-4.5zM6.437 4.758A.5.5 0 0 0 6 4.5h-.066L8 1.401 10.066 4.5H10a.5.5 0 0 0-.424.765L11.598 8.5H11.5a.5.5 0 0 0-.447.724L12.69 12.5H3.309l1.638-3.276A.5.5 0 0 0 4.5 8.5h-.098l2.022-3.235a.5.5 0 0 0 .013-.507z" />
           </svg>
         </div>
