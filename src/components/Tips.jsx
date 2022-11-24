7c04e0459779ad1208796949df5a3a32224100a6
Victor Ly <vict4222@stud.kea.dk>
2022-11-24 12:09:47 +0100

    CO2 subscript

--- src/components/Tips.jsx
+++ src/components/Tips.jsx
@@ -6,14 +6,22 @@ import React from "react";
 function Tips(props) {
   return (
     <section id="tips">
-      <h2>Advice to reduce your CO2 emissions</h2>
+      <h2>
+        Advice to reduce your CO<sub>2</sub> emissions
+      </h2>
       <div className="tips_container">
         <TipsBox title="Browser" text="Close unused tabs in your browsers." />
         <TipsBox title="Apps" text="Close apps you don't use so they aren't running in the background." />
-        <TipsBox title="Emails" text="Unsubscribe to newsletter you don't read. Don’t use reply all unless necessary." />
+        <TipsBox
+          title="Emails"
+          text="Unsubscribe to newsletter you don't read. Don’t use reply all unless necessary."
+        />
         <TipsBox title="Sleepmode" text="Turn off your devices any time you won’t be using them for 2 hours or more." />
         <TipsBox title="Charging" text="Don’t leave your devices plugged in for longer than they need to charge." />
-        <TipsBox title="Turn off autoplay" text="Minimize unnecessary energy use by avoiding streaming videos you’re not actually watching." />
+        <TipsBox
+          title="Turn off autoplay"
+          text="Minimize unnecessary energy use by avoiding streaming videos you’re not actually watching."
+        />
         <TipsBox title="Clouds" text="Move to the cloud and choose a cloud provider that uses renewable energy." />
         <TipsBox
           title="Booksmarks"
@@ -21,7 +29,6 @@ function Tips(props) {
             time."
         />
         <TipsBox title="Deleting" text="Purge your inbox, photo albums, documents, and other files regularly." />
-
       </div>
     </section>
   );
